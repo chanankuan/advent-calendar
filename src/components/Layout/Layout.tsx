f5e170159e046bb96ce5b1d78600a9ef497121ae
Anton Chan <106675165+chanankuan@users.noreply.github.com>
2024-12-04 12:59:37 +0100

    Update layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header/Header";
 
 function Layout() {
+  const location = useLocation();
+  const isAdvent = location.pathname.startsWith("/advent-calendar/");
+
   return (
-    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Header />
 
-      <main style={{ position: "relative", flex: 1 }}>
+      <main
+        style={{
+          position: "relative",
+          flex: 1,
+          paddingTop: !isAdvent ? "84px" : undefined,
+        }}
+      >
         <Outlet />
       </main>
     </div>
