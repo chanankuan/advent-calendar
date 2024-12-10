eb54b4eb6310ea41b4d6ba5ff50c3c6f4ac54b00
Anton Chan <106675165+chanankuan@users.noreply.github.com>
2024-12-10 17:55:50 +0100

    fix bug

--- src/helpers/handleAxiosError.ts
+++ src/helpers/handleAxiosError.ts
@@ -6,7 +6,7 @@ export function handleAxiosError(
   fallbackMessage = "An unexpected error occurred."
 ) {
   if (axios.isAxiosError(error)) {
-    const message = error.response?.data?.error || fallbackMessage;
+    const message = error.response?.data || fallbackMessage;
 
     toast.error(message, {
       position: "top-right",
